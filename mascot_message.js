d3cac022ee164e9cba0a6fd579431b7fd9b5a5c9
mihaelacsn <74179246+mihaelacsn@users.noreply.github.com>
2024-03-14 22:28:43 +0200

    Added the page mascot.

---
+++ mascot_message.js
@@ -0,0 +1,7 @@
+mascot.addEventListener('mouseenter', function() {
+  mascotMessage.style.display = 'block';
+});
+
+mascot.addEventListener('mouseleave', function() {
+  mascotMessage.style.display = 'none';
+});
