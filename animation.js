db0f7bd6d8fb8f305b41ce0123c5657003d9a35b
mihaelacsn <74179246+mihaelacsn@users.noreply.github.com>
2024-03-14 22:28:43 +0200

    Added the page mascot.

---
+++ animation.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    setTimeout(function() {
+      var mascot = document.getElementById('mascot');
+      if (mascot) {
+        mascot.style.display = 'block';
+      }
+    }, 5000); 
+  };
+
+  
